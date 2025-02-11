7e490ac3b18d2b11a1286fed7939df07bb25185b
ElijahB09 <134438080+ElijahB09@users.noreply.github.com>
2025-02-11 12:17:26 -0600

    Remove qual from this repo (#3)

--- tests/test_certificate_dns_record_provider.py
+++ tests/test_certificate_dns_record_provider.py
@@ -162,7 +162,7 @@ class Request(dict):
             {
                 "RequestType": request_type,
                 "ResponseURL": "https://httpbin.org/put",
-                "StackId": "arn:aws:cloudformation:us-west-2:EXAMPLE/stack-name/guid",
+                "StackId": "arn:aws:cloudformation:us-east-1:EXAMPLE/stack-name/guid",
                 "RequestId": request_id,
                 "ResourceType": "Custom::CertificateDNSRecord",
                 "LogicalResourceId": "Record",
